c15ef1764845ae845cdd888c19ad35127831a4e7
Mfahad159 <mianfahad664@gmail.com>
2025-07-24 00:23:50 +0500

    Sidebar Completed

--- NeuroSync/src/components/sidebar/Sidebar.jsx
+++ NeuroSync/src/components/sidebar/Sidebar.jsx
@@ -1,38 +1,50 @@
+import { useState } from 'react'
 import React from 'react'
 import { assets } from '../../assets/assets.js'
 import './Sidebar.css'
 const Sidebar = () => {
+
+    const [extended, setExtended] = React.useState(false);
+    const handle_exetrended = () => {
+        setExtended(!extended);
+    }
   return (
-    <div className='sidebar'>
-        <div className="top">
-                <img className='menu' src={assets.menu_icon} alt="" />
-                <div className='new-chat'>
-                    <img src={assets.plus_icon} alt="" />
-                    <p className='new-chat-title'>New Chat</p>
-                </div>
-                <div className='recent'>
-                    <div className='recent-title'></div>
-                    <div className='recent-entry'>
-                        <img src={assets.message_icon} alt="" />
-                        <p>What is React....</p>
-                    </div>
-                </div>
-        </div>
-        <div className='bottom'>
-            <div>
-                <img src={assets.question_icon} alt="" />
-                <p>Help</p>
-            </div>
-            <div>
-                <img src={assets.history_icon} alt="" />
-                <p>History</p>
-            </div>
-            <div>
-                <img src={assets.setting_icon} alt="" />
-                <p>Settings</p>
-            </div>
-        </div>
+   <div className={`sidebar ${extended ? 'sidebar-extended' : 'sidebar-collapsed'}`}>
+  <div className="top">
+    <img className='menu' onClick={handle_exetrended} src={assets.menu_icon} alt="" />
+    <div className='new-chat'>
+      <img src={assets.plus_icon} alt="" />
+      {extended && <p>New Chat</p>}
     </div>
+    {extended &&
+      <div className='recent'>
+        <div className='recent-title'></div>
+        <div className='recent-entry'>
+          <img src={assets.message_icon} alt="" />
+          <p>What is React....</p>
+        </div>
+      </div>
+    }
+  </div>
+
+  {extended &&
+    <div className='bottom'>
+      <div className='bottom-item recent-entry'>
+        <img src={assets.question_icon} alt="" />
+        <p>Help</p>
+      </div>
+      <div className='bottom-item recent-entry'>
+        <img src={assets.history_icon} alt="" />
+        <p>History</p>
+      </div>
+      <div className='bottom-item recent-entry'>
+        <img src={assets.setting_icon} alt="" />
+        <p>Settings</p>
+      </div>
+    </div>
+  }
+</div>
+
   )
 }
 
